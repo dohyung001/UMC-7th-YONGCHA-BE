845ef877837959be14ff057137d6aa1a3f722a7a
김용민 <dydals3440@gmail.com>
2024-09-29 22:04:22 +0900

    feat: 7th Auth Refresh/Access Token 구현

---
+++ src/app.module.ts
@@ -0,0 +1,51 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entity/user.entity';
+import { AuthModule } from './auth/auth.module';
+import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
+import { JwtService } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'yongcha-auth.sqlite',
+      entities: [User],
+      synchronize: true,
+      logging: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, JwtService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(BearerTokenMiddleware)
+      .exclude(
+        {
+          path: 'auth/login',
+          method: RequestMethod.POST,
+        },
+        {
+          path: 'auth/register',
+          method: RequestMethod.POST,
+        },
+      )
+      .forRoutes('*');
+  }
+}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Post, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // 1. 회원가입
+  @Post('register')
+  registerUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
+  }
+
+  // 2. 로그인
+  @Post('login')
+  loginUser(@Body() createUserDto: Omit<CreateUserDto, 'passwordCheck'>) {
+    return this.authService.login(createUserDto);
+  }
+
+  // 3. 토큰 재발급
+  @Post('token/access')
+  async rotateAccessToken(@Request() req) {
+    console.log(req);
+    return {
+      accessToken: await this.authService.issueToken(req.user, false),
+      refreshToken: await this.authService.issueToken(req.user, true),
+    };
+  }
+}
